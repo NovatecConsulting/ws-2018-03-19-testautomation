44dc66786816639773a436b2c74ff42ba06f1d95
Stefan Ludwig <stefan.ludwig@novatec-gmbh.de>
2018-03-19 09:41:10 +0100

    initial applications

---
+++ servicea/src/main/kotlin/com/example/servicea/ServiceaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.servicea
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServiceaApplication
+
+fun main(args: Array<String>) {
+    runApplication<ServiceaApplication>(*args)
+}
---
+++ serviceb/src/main/kotlin/com/example/serviceb/ServicebApplication.kt
@@ -0,0 +1,11 @@
+package com.example.serviceb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServicebApplication
+
+fun main(args: Array<String>) {
+    runApplication<ServicebApplication>(*args)
+}
